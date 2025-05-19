57d5a482cdb1d0981e0d549bc07af1c3a07504c7
Your Name <roycodeman@gmail.com>
2025-05-19 19:35:36 +0530

    Updated code

---
+++ 3. Pure functions/index.js
@@ -0,0 +1,12 @@
+// ! What is pure functions ?
+// * Nisha Singla ==> https://www.youtube.com/watch?v=gHpEpw5nEGs&list=PLC8OkhrVTHNFITtT_TzfBbn8SohEU8Cpa&index=4&ab_channel=NishaSingla
+
+// Predictable && Readble && Resuable && Testable
+function pure(x, y) {
+    return x + y;
+}
+
+console.log(pure(10, 20));
+console.log(pure(10, 20));
+console.log(pure(10, 20));
+console.log(pure(10, 20));
