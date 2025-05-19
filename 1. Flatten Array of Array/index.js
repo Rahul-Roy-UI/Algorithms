5c952fe3e1ddb737d72701ce0eab5cc866577c34
Your Name <roycodeman@gmail.com>
2025-05-19 18:19:39 +0530

    Closure

--- 1. Flatten Array of Array/index.js
+++ 1. Flatten Array of Array/index.js
@@ -1,5 +1,5 @@
-// https://www.youtube.com/watch?v=dBk5CJYSr5k&list=PLC8OkhrVTHNFITtT_TzfBbn8SohEU8Cpa&ab_channel=NishaSingla
-// How to Flatten Array of Array
+// * Nisha Singla ==> https://www.youtube.com/watch?v=dBk5CJYSr5k&list=PLC8OkhrVTHNFITtT_TzfBbn8SohEU8Cpa&ab_channel=NishaSingla
+// ! How to Flatten Array of Array
 
 let arr = [1, 2, [3, 4, 5], 6, [7, 8, [9, 10, 11]]];
 let output = [];
---
+++ 2. Closures/index.js
@@ -0,0 +1,18 @@
+// * Akshay Saini ==> https://www.youtube.com/watch?v=qikxEIxsXco&list=PLlasXeu85E9cQ32gLCvAvr9vNaUccPVNP&index=12&ab_channel=AkshaySaini
+// ! What are Closures ?
+// * A function along with it's lexical scope together bundled froms a closure
+// ! use debugger to learn more
+// ! **Read** ==> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Closures
+
+function x() {
+    var a = 7;
+    function y() {
+        console.log(a);
+    }
+
+    return y;
+}
+
+var z = x();
+console.log(z);
+z();
