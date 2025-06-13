f79beedcc9b7951d8d02e99118fe699331cca252
Rahul Roy <roycodeman@gmail.com>
2025-06-13 14:54:46 +0530

    Updated code and renaming of folders

---
+++ 09. Reverse Array In Place/index.js
@@ -0,0 +1,14 @@
+// ! reverseArrayInPlace
+// https://www.udemy.com/course/learning-algorithms-in-javascript-from-scratch/learn/lecture/7723712#overview 
+
+function reverseArrayInPlace(arr) {
+  for (var i = 0; i < arr.length / 2; i++) {
+    var tempVar = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = tempVar;
+  }
+
+  return arr;
+}
+
+reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7, 8]);
